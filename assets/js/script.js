245e398095303815f42ec2f70af4c8a4743298a6
Tyler Norman <tnorman600@gmail.com>
2021-10-18 13:06:36 -0600

    a usert can now click any part of their search result and an event listener will trigger. Later on, this even listener will be used to get the title ID of the movie/show the user selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,29 +31,56 @@ let run = function(event) {
                 removeAllChildNodes(searchResults)
                 // iterate over movie database search results and display 20 results in search modal
                 for (let i = 0; i < 10; i++) {
+                    // create elements for results to reside in
                     var resultDiv = document.createElement(`div`);
                     var posterImg = document.createElement(`img`);
                     var infoDiv = document.createElement(`div`);
                     var titleSpan = document.createElement(`span`);
                     var typeAndYearSpan = document.createElement(`span`);
+                    var titleIDSpan = document.createElement(`span`);
 
+                    // add class="result" to each element inside an individual search result for later use in event listener below
+                    resultDiv.classList.add("result");
+                    posterImg.classList.add("result");
+                    infoDiv.classList.add("result");
+                    titleSpan.classList.add("result");
+                    typeAndYearSpan.classList.add("result");
+                    titleIDSpan.classList.add("result");
+                    titleIDSpan.classList.add("titleID");
+
+                    // set poster src for each search result
                     posterImg.src = `https://image.tmdb.org/t/p/w500` + data.results[i].poster_path;
 
+                    // if search result item is a 'movie' type
                     if (data.results[i].title) {
+                        // assign movie title, type, release year, and movie ID to variables
                         var showTitle = document.createTextNode(data.results[i].title);
-                        var showTypeAndYear = document.createTextNode(data.results[i].media_type + `, ` + data.results[i].release_date.substring(0, 4));
+                        var showTypeAndYear = document.createTextNode(data.results[i].media_type + `, ` + data.results[i].release_date.substring(0, 4) + " ID: ");
+                        var titleID = document.createTextNode(data.results[i].id);
                     } else {
+                        // search result item type is 'tv'; assign tv title, type, release year, and tv ID to variables
                         var showTitle = document.createTextNode(data.results[i].name);
-                        var showTypeAndYear = document.createTextNode(data.results[i].media_type + `, ` + data.results[i].first_air_date.substring(0, 4));
+                        var showTypeAndYear = document.createTextNode(data.results[i].media_type + `, ` + data.results[i].first_air_date.substring(0, 4) + " ID: ");
+                        var titleID = document.createTextNode(data.results[i].id);
                     }
+                    // append title, media type, release year, and ID variables to elements that were dynamically created above
                     resultDiv.appendChild(posterImg);
                     resultDiv.appendChild(infoDiv)
                     infoDiv.appendChild(titleSpan);
                     infoDiv.appendChild(typeAndYearSpan);
+                    infoDiv.appendChild(titleIDSpan)
+                    titleSpan.appendChild(showTitle)
                     typeAndYearSpan.appendChild(showTypeAndYear);
-                    titleSpan.appendChild(showTitle);
+                    titleIDSpan.appendChild(titleID);
                     searchResults.appendChild(resultDiv);
                 }
+
+                document.addEventListener('click',function(e){
+                    if(e.target.classList == "result"){
+                        alert("click");
+                    }
+                });
+                
                 showID = (data.results[0].id); // Set id to MovieDB ID
                 console.log(data.results[0].original_title); // Log MovieDB official title
             } catch {
