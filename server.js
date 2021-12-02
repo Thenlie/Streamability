f538035f55612c917fe6dc74cf4f6b57dc5b40ab
Leithen <leithen113@gmail.com>
2021-12-01 22:11:36 -0700

    Another CORS test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors());
+// app.use(cors({ origin: 'https://streamability.herokuapp.com/', credentials: true }));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(express.static('public'));
