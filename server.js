0d17fe68966b5bd0a2e4c1bfc21c37244313e73e
Leithen <leithen113@gmail.com>
2021-12-01 21:55:36 -0700

    testing cors issue

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-// const cors_proxy = require('cors-anywhere');
-// const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(cors({ origin: 'https://streamability.herokuapp.com/', credentials: true }));
+app.use(cors({ origin: 'https://streamability.herokuapp.com/' }));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -15,12 +12,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-// cors_proxy.createServer({
-//     originWhitelist: [],
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2']
-// })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
