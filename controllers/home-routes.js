d0457bb955321da0d88db36f86602f5e02a98940
Leithen <leithen113@gmail.com>
2022-01-11 22:39:30 -0700

    Fixed search bar on info page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,7 @@ router.get('/info/:type/:title/:id', async (req, res) => {
     const infoRes = await fetch(`https://api.themoviedb.org/3/${req.params.type}/${req.params.id}?api_key=${process.env.MOVIEDB_KEY}`);
     const watchRes = await fetch(`https://api.themoviedb.org/3/${req.params.type}/${req.params.id}/watch/providers?api_key=${process.env.MOVIEDB_KEY}`);
     const suggestionRes = await fetch(`https://hidden-retreat-58836.herokuapp.com/https://tastedive.com/api/similar?q=${req.params.title}&type=${req.params.type}&limit=5&info=1&k=${process.env.TASTEDIVE_KEY}`, { headers: {"X-Requested-With": "XMLHttpRequest"}});
-    const queueRes = await fetch('/api/queue');
+    const queueRes = await fetch(`http://localhost:3001/api/queue/${req.session.user_id}`);
 
     const info = await infoRes.json();
     const watchProviders = await watchRes.json();
