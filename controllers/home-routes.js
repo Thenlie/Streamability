ced2610e1f05400e3ca7d4d36052474cb38bb486
Leithen <leithen113@gmail.com>
2022-04-20 20:33:01 -0600

    Changed Tastedive API to MovieDB

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,16 +10,18 @@ router.get('/', (req, res) => {
 router.get('/info/:type/:title/:id', async (req, res) => {
     const infoRes = await fetch(`https://api.themoviedb.org/3/${req.params.type}/${req.params.id}?api_key=${process.env.MOVIEDB_KEY}`);
     const watchRes = await fetch(`https://api.themoviedb.org/3/${req.params.type}/${req.params.id}/watch/providers?api_key=${process.env.MOVIEDB_KEY}`);
-    const suggestionRes = await fetch(`https://hidden-retreat-58836.herokuapp.com/https://tastedive.com/api/similar?q=${req.params.title}&type=${req.params.type}&limit=5&info=1&k=${process.env.TASTEDIVE_KEY}`, { headers: {"X-Requested-With": "XMLHttpRequest"}});
+    const suggestionRes = await  fetch(`https://api.themoviedb.org/3/${req.params.type}/${req.params.id}/recommendations?api_key=${process.env.MOVIEDB_KEY}&limit=5`);
     const queueRes = await fetch(`http://localhost:3001/api/queue/${req.session.user_id}`);
 
     const info = await infoRes.json();
     const watchProviders = await watchRes.json();
-    const suggestions = await suggestionRes.json();
+    const suggestionData = await suggestionRes.json();
     const queue = await queueRes.json();
     const providers = watchProviders.results.US.flatrate;
-    console.log(queue)
-    console.log(req.session.user_id)
+    let suggestions = [];
+    for (let i = 0; i < 5; i++) {
+        suggestions.push(suggestionData.results[i]);
+    };
     res.render('info', { info, providers, suggestions, queue, loggedIn: req.session.loggedIn })
 })
 
