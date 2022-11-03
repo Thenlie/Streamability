fc23ab6c0e5159e01ba7461c85e5c482b9f2c71f
Thenlie <leithen113@gmail.com>
2022-11-02 22:53:21 -0600

    Install React Router. Set up basic route. Create PageNoteFound screen.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import PageNotFound from './screens/PageNotFound'
+
+/**
+ * Create the 'root route' and serve the entire app to it
+ */
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <PageNotFound />,
+    }
+])
 
 /**
  * This code was added with the Vite boiler plate
@@ -11,6 +24,6 @@ import './index.css'
  */
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/screens/PageNotFound.tsx
@@ -0,0 +1,29 @@
+import { ErrorResponse } from "@remix-run/router";
+import { useRouteError } from 'react-router-dom';
+
+/**
+ * @returns tsx of 404 page
+ */
+function PageNotFound() {
+    /**
+     * This hook returns anything thrown during an 
+     * action, loader, or rendering
+     */
+    const error: ErrorResponse = useRouteError() as ErrorResponse;
+    console.error(error);
+    
+    /**
+     * @TODO Implement better error handling
+     * @TODO Handle thrown responses with 'isRouteErrorResponse'
+     * 
+     * https://reactrouter.com/en/main/route/error-element#throwing-responses
+     */
+    return (
+        <div>
+            <h1>Page Not Found!</h1>
+            <p>{ error.statusText }</p>
+        </div>
+    )
+}
+
+export default PageNotFound;
