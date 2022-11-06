a689b2d62f7185f389cecfe720e9502401a7a09e
maxh1231 <maxhumpherys@gmail.com>
2022-11-05 23:30:52 -0600

    OutLetContext for root level session state

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,6 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 
+import { useState, useEffect } from 'react'
+import { supabase } from './helpers/supabaseClient'
+import Login from './screens/LoginScreen'
+import Dashboard from './screens/Dashboard'
+
 /**
  * The main app function, wrapping all other screens and components
  * This wraps the entire front end application and will be shown on every screen
@@ -8,12 +13,26 @@ import './App.css';
  * @returns Streamability!
  */
 export default function AppWrapper() {
+	const [session, setSession] = useState(null)
+
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session)
+		})
+
+		supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session)
+		})
+	}, [])
+
+
+
 	return (
 		<div className="App">
 			<h1>Streamability</h1>
 			<div>
-				<Outlet />
-			</div>    
+				<Outlet context={[session, setSession]} />
+			</div>
 		</div>
 	);
 }
