9473cc5b701e89021d7f28cacb1863c3e2bea8e5
maxh1231 <maxhumpherys@gmail.com>
2022-11-06 20:17:01 -0700

    Fixed several errors. Imported useOutletContext and defined Types in AppWrapper, passing session state through useSession() which is imported and invoked in Dashboard component

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import './App.css';
 
 import { useState, useEffect } from 'react'
@@ -12,27 +12,52 @@ import Dashboard from './screens/Dashboard'
  * 
  * @returns Streamability!
  */
-export default function AppWrapper() {
-	const [session, setSession] = useState(null)
 
+interface User {
+	id: string,
+	created_at: string,
+	email: string,
+	role: string,
+	updated_at: string
+}
+
+interface Session {
+	access_token: string,
+	expires_at: number,
+	expires_in: number,
+	refresh_token: string,
+	token_type: string,
+	user: User;
+}
+
+type ContextType = { session: Session | null }
+export default function AppWrapper() {
+
+
+	const [session, setSession] = useState<Session | null>(null);
 	useEffect(() => {
 		supabase.auth.getSession().then(({ data: { session } }) => {
-			setSession(session)
+			setSession(session as Session)
 		})
 
 		supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session)
+			setSession(session as Session)
 		})
 	}, [])
 
-
-
+	console.log(session);
 	return (
 		<div className="App">
 			<h1>Streamability</h1>
 			<div>
-				<Outlet context={[session, setSession]} />
+				<Outlet context={{ session }} />
 			</div>
 		</div>
 	);
+}
+
+
+
+export function useSession() {
+	return useOutletContext<ContextType>();
 }
--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -12,8 +12,6 @@ export default function SignupScreen() {
             email: email,
             password: password,
         })
-
-
         console.log(email, password)
     }
 
