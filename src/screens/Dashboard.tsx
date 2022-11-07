8213e52ffae32f2aeb7f03398cd395b5f0764a1f
maxh1231 <maxhumpherys@gmail.com>
2022-11-06 20:17:01 -0700

    Fixed several errors. Imported useOutletContext and defined Types in AppWrapper, passing session state through useSession() which is imported and invoked in Dashboard component

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,106 +1,17 @@
 import { useState, useEffect } from 'react'
 import { useOutletContext } from 'react-router-dom'
 import { supabase } from '../helpers/supabaseClient'
+import { useSession } from '../AppWrapper'
 
 const Dashboard = () => {
-    const [session, setSession] = useOutletContext();
-    const [loading, setLoading] = useState(true)
-    const [username, setUsername] = useState(null)
-    const [website, setWebsite] = useState(null)
-    const [avatar_url, setAvatarUrl] = useState(null)
-
+    const { session } = useSession();
     console.log(session)
-
-    useEffect(() => {
-        getProfile()
-    }, [session])
-
-    const getProfile = async () => {
-        try {
-            setLoading(true)
-            const { user } = session
-
-            let { data, error, status } = await supabase
-                .from('profiles')
-                .select(`username`)
-                .eq('id', user.id)
-                .single()
-
-            if (error && status !== 406) {
-                throw error
-            }
-
-            if (data) {
-                setUsername(data.username)
-                setWebsite(data.website)
-                setAvatarUrl(data.avatar_url)
-            }
-        } catch (error) {
-            alert(error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    const updateProfile = async (e) => {
-        e.preventDefault()
-
-        try {
-            setLoading(true)
-            const { user } = session
-
-            const updates = {
-                id: user.id,
-                username,
-                website,
-                avatar_url,
-                updated_at: new Date(),
-            }
-
-            let { error } = await supabase.from('profiles').upsert(updates)
-
-            if (error) {
-                throw error
-            }
-        } catch (error) {
-            alert(error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
 
     return (
         <div aria-live="polite">
-            {loading ? (
-                'Saving ...'
-            ) : (
-                <form onSubmit={updateProfile} className="form-widget">
-                    <div>Email: {session.user.email}</div>
-                    <div>
-                        <label htmlFor="username">Name</label>
-                        <input
-                            id="username"
-                            type="text"
-                            value={username || ''}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="website">Website</label>
-                        <input
-                            id="website"
-                            type="url"
-                            value={website || ''}
-                            onChange={(e) => setWebsite(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <button className="button primary block" disabled={loading}>
-                            Update profile
-                        </button>
-                    </div>
-                </form>
-            )}
+            <form className="form-widget">
+                <div>Email: {session?.user.email}</div>
+            </form>
             <button type="button" className="button block" onClick={() => supabase.auth.signOut()}>
                 Sign Out
             </button>
