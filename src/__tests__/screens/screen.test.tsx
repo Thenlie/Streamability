1a01b25060ef9b30e872684958e1fd0f4a097694
Thenlie <leithen113@gmail.com>
2022-12-18 21:33:06 -0700

    Improve search results test. Create show details screen test

--- src/__tests__/screens/screen.test.tsx
+++ src/__tests__/screens/screen.test.tsx
@@ -6,6 +6,19 @@ import { routes } from '../routes';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
 
+// mock MovieDB API calls
+vi.mock('../../helpers/getMovieUtils', () => {
+    const sampleMovieData = vi.importActual('./assets/searchData.json');
+    const sampleMovieDetailsData = vi.importActual('./assets/movieData.json');
+    const sampleMovieProviders = vi.importActual('./assets/providerData.json');
+    return {
+        default: {},
+        getMoviesByName: vi.fn().mockResolvedValue(sampleMovieData),
+        getMovieDetails: vi.fn().mockResolvedValue(sampleMovieDetailsData),
+        getMovieProviders: vi.fn().mockResolvedValue(sampleMovieProviders)
+    };
+});
+
 describe('Screen Test Suite', async () => {
     // set up variables to be used on each test
     let user: UserEvent;
@@ -49,17 +62,6 @@ describe('Screen Test Suite', async () => {
         });
         // render screens
         render(<RouterProvider router={router}/>);
-
-        // mock MovieDB API calls
-        vi.mock('../../helpers/getMovieUtils', () => {
-            const sampleMovieData = vi.importActual('./assets/searchData.json');
-            const sampleMovieDetailsData = vi.importActual('./assets/movieData.json');
-            return {
-                default: {},
-                getMoviesByName: vi.fn().mockResolvedValue(sampleMovieData),
-                getMovieDetails: vi.fn().mockResolvedValue(sampleMovieDetailsData),
-            };
-        });
         
         // check landing page
         await waitFor(() => screen.getByTestId('featured-search-heading'));
@@ -71,6 +73,29 @@ describe('Screen Test Suite', async () => {
         // navigate to search results page
         await user.click(screen.getByTestId('featured-search-button'));
         await waitFor(() => screen.getByTestId('search-results-heading'));
-        expect(screen.getByTestId('search-results-heading')).toBeInTheDocument(); 
+        expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Search Results Page');
+        // check for show card
+        expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Iron Man');
+        // check for queue buttons
+        expect(screen.getAllByRole('button')[1]).toHaveTextContent('Add to queue');
+        expect(screen.getAllByRole('button')[2]).toHaveTextContent('Remove from queue');
+    });
+    it('navigates to show details screen when show card is clicked on', async () => {
+        // create a new data router for the test
+        const router = createMemoryRouter(routes, {
+            initialEntries: ['/search?q=iron+man']
+        });
+        // render screens
+        render(<RouterProvider router={router}/>);
+
+        await waitFor(() => screen.getByTestId('search-results-heading'));
+        expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Search Results Page');
+        // navigate to details screen
+        await user.click(screen.getAllByTestId('show-details-link')[0]);
+        await waitFor(() => screen.getByTestId('show-details-heading'));
+        expect(screen.getByTestId('details-provider')).toBeInTheDocument();
+        expect(screen.getByTestId('details-provider')).toHaveTextContent('Disney Plus');
+        expect(screen.getByTestId('details-release-date')).toBeInTheDocument();
+        expect(screen.getByTestId('details-release-date')).toHaveTextContent('April 30th, 2008');
     });
 });
