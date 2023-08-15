282d662922275191a7af41d3834a54f08b5f4c89
maxh1231 <maxhumpherys@gmail.com>
2023-08-14 21:26:58 -0600

    ShowCarouselPlaceholder story. Code review

---
+++ src/stories/components/ShowCarouselPlaceholder.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ShowCarouselPlaceholder } from '../../components';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+const meta = {
+    title: 'Components/Show Carousel Placeholder',
+    component: ShowCarouselPlaceholder,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    },
+    decorators: [withRouter],
+} satisfies Meta<typeof ShowCarouselPlaceholder>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CarouselPlaceholder: Story = {
+    args: {
+        count: 5,
+    },
+};
