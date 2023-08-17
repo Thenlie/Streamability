ac24dcba5f5ddd3fd54ce9061975bffe6538f069
Thenlie <leithen113@gmail.com>
2023-08-16 22:45:05 -0600

    create filter by released after function and unit tests

--- src/helpers/__tests__/showFilter.test.ts
+++ src/helpers/__tests__/showFilter.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import {
     filterShowsByGenre,
+    filterShowsByReleasedAfter,
     filterShowsByReleasedBefore,
     filterShowsByType,
 } from '../showFilterUtils';
@@ -23,7 +24,17 @@ describe('filterShowsByType', () => {
 });
 
 describe('filterShowsByReleasedBefore', () => {
-    it('properly filters shows based on a specified genre id', () => {
+    it('properly filters shows released on or before the specified date', () => {
         expect(filterShowsByReleasedBefore(SHOW_DATA_ARRAY, '2000-01-01').length).toBe(12);
+        expect(filterShowsByReleasedBefore(SHOW_DATA_ARRAY, '2015-03-09').length).toBe(27);
+        expect(filterShowsByReleasedBefore(SHOW_DATA_ARRAY, '1940-01-01').length).toBe(1);
+    });
+});
+
+describe('filterShowsByReleasedAfter', () => {
+    it('properly filters shows released on or after the specified date', () => {
+        expect(filterShowsByReleasedAfter(SHOW_DATA_ARRAY, '2000-01-01').length).toBe(18);
+        expect(filterShowsByReleasedAfter(SHOW_DATA_ARRAY, '2015-03-11').length).toBe(2);
+        expect(filterShowsByReleasedAfter(SHOW_DATA_ARRAY, '1940-01-01').length).toBe(29);
     });
 });
--- src/helpers/showFilterUtils.ts
+++ src/helpers/showFilterUtils.ts
@@ -45,4 +45,34 @@ const filterShowsByReleasedBefore = (showData: ShowData[], releaseDate: string):
     return filteredShows;
 };
 
-export { filterShowsByGenre, filterShowsByType, filterShowsByReleasedBefore };
+const filterShowsByReleasedAfter = (showData: ShowData[], releaseDate: string): ShowData[] => {
+    const filteredShows: ShowData[] = [];
+    const targetDate = new Date(releaseDate);
+    showData.forEach((show) => {
+        if (!show.release_date) return;
+        const showDate = new Date(show.release_date);
+        if (showDate.getFullYear() > targetDate.getFullYear()) {
+            filteredShows.push(show);
+            return;
+        }
+        if (showDate.getFullYear() === targetDate.getFullYear()) {
+            if (showDate.getMonth() > targetDate.getMonth()) {
+                filteredShows.push(show);
+                return;
+            }
+            if (showDate.getMonth() === targetDate.getMonth()) {
+                if (showDate.getDay() >= targetDate.getDay()) {
+                    filteredShows.push(show);
+                }
+            }
+        }
+    });
+    return filteredShows;
+};
+
+export {
+    filterShowsByGenre,
+    filterShowsByType,
+    filterShowsByReleasedBefore,
+    filterShowsByReleasedAfter,
+};
