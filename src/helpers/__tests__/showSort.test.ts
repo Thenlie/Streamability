d02e840bedc49676c864cf696b6d0cb6373a29f9
Thenlie <leithen113@gmail.com>
2023-08-25 22:47:42 -0600

    #450 create sort show by release date functions and tests

--- src/helpers/__tests__/showSort.test.ts
+++ src/helpers/__tests__/showSort.test.ts
@@ -4,6 +4,8 @@ import {
     sortShowsByAvgRatingDesc,
     sortShowsByRatingCountAsc,
     sortShowsByRatingCountDesc,
+    sortShowsByReleaseDateAsc,
+    sortShowsByReleaseDateDesc,
 } from '../showSortUtils';
 import { SHOW_DATA_ARRAY } from './constants';
 
@@ -42,5 +44,24 @@ describe('sortShowsByAvgRating', () => {
         expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[30].vote_average).toBe(0);
         expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[29].vote_average).toBe(0);
         expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[28].vote_average).toBe(0);
+    });
+});
+
+describe('sortShowsByReleaseDate', () => {
+    it('properly sorts shows by release date ascending', () => {
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[0].release_date).toBe('1931-04-29');
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[1].release_date).toBe('1951-09-20');
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[2].release_date).toBe('1963-10-20');
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[30].release_date).toBe('2017-10-17');
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[29].release_date).toBe('2017-05-25');
+        expect(sortShowsByReleaseDateAsc(SHOW_DATA_ARRAY)[28].release_date).toBe('2015-03-10');
+    });
+    it('properly sorts shows by release date descending', () => {
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[0].release_date).toBe('2017-10-17');
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[1].release_date).toBe('2017-05-25');
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[2].release_date).toBe('2015-03-10');
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[30].release_date).toBe('1931-04-29');
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[29].release_date).toBe('1951-09-20');
+        expect(sortShowsByReleaseDateDesc(SHOW_DATA_ARRAY)[28].release_date).toBe('1963-10-20');
     });
 });
--- src/helpers/showSortUtils.ts
+++ src/helpers/showSortUtils.ts
@@ -3,7 +3,6 @@ import { ShowData } from '../types';
 /**
  * TODO:
  * - Release Date [H, L]
- * - Genre [A-Z]
  */
 
 /**
@@ -50,9 +49,37 @@ const sortShowsByAvgRatingDesc = (showData: ShowData[]): ShowData[] => {
     return showData;
 };
 
+/**
+ * Sort an array of shows based on the release date.
+ * The oldest date will be first, the newest will be last
+ * @param showData | array of shows to be sorted
+ * @returns | array of sorted shows
+ */
+const sortShowsByReleaseDateAsc = (showData: ShowData[]): ShowData[] => {
+    showData.sort(
+        (a, b) => new Date(a.release_date || 0).getTime() - new Date(b.release_date || 0).getTime()
+    );
+    return showData;
+};
+
+/**
+ * Sort an array of shows based on the release date.
+ * The newest date will be first, the oldest will be last
+ * @param showData | array of shows to be sorted
+ * @returns | array of sorted shows
+ */
+const sortShowsByReleaseDateDesc = (showData: ShowData[]): ShowData[] => {
+    showData.sort(
+        (a, b) => new Date(b.release_date || 0).getTime() - new Date(a.release_date || 0).getTime()
+    );
+    return showData;
+};
+
 export {
     sortShowsByRatingCountAsc,
     sortShowsByRatingCountDesc,
     sortShowsByAvgRatingAsc,
     sortShowsByAvgRatingDesc,
+    sortShowsByReleaseDateAsc,
+    sortShowsByReleaseDateDesc,
 };
