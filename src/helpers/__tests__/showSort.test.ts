598aaacef59f64b598b58faab0dc4a4b477acc22
Thenlie <leithen113@gmail.com>
2023-08-25 22:29:54 -0600

    #450 create sort show by vote average functions and tests

--- src/helpers/__tests__/showSort.test.ts
+++ src/helpers/__tests__/showSort.test.ts
@@ -1,10 +1,17 @@
 import { describe, expect, it } from 'vitest';
-import { sortShowsByRatingCountAsc, sortShowsByRatingCountDesc } from '../showSortUtils';
+import {
+    sortShowsByAvgRatingAsc,
+    sortShowsByAvgRatingDesc,
+    sortShowsByRatingCountAsc,
+    sortShowsByRatingCountDesc,
+} from '../showSortUtils';
 import { SHOW_DATA_ARRAY } from './constants';
 
 describe('sortShowsByRatingCount', () => {
     it('properly sorts shows by vote count ascending', () => {
         expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[0].vote_count).toBe(0);
+        expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[1].vote_count).toBe(0);
+        expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[2].vote_count).toBe(0);
         expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[30].vote_count).toBe(24715);
         expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[29].vote_count).toBe(20941);
         expect(sortShowsByRatingCountAsc(SHOW_DATA_ARRAY)[28].vote_count).toBe(19625);
@@ -14,5 +21,26 @@ describe('sortShowsByRatingCount', () => {
         expect(sortShowsByRatingCountDesc(SHOW_DATA_ARRAY)[1].vote_count).toBe(20941);
         expect(sortShowsByRatingCountDesc(SHOW_DATA_ARRAY)[2].vote_count).toBe(19625);
         expect(sortShowsByRatingCountDesc(SHOW_DATA_ARRAY)[30].vote_count).toBe(0);
+        expect(sortShowsByRatingCountDesc(SHOW_DATA_ARRAY)[29].vote_count).toBe(0);
+        expect(sortShowsByRatingCountDesc(SHOW_DATA_ARRAY)[28].vote_count).toBe(0);
+    });
+});
+
+describe('sortShowsByAvgRating', () => {
+    it('properly sorts shows by average rating ascending', () => {
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[0].vote_average).toBe(0);
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[1].vote_average).toBe(0);
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[2].vote_average).toBe(0);
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[30].vote_average).toBe(10);
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[29].vote_average).toBe(8.4);
+        expect(sortShowsByAvgRatingAsc(SHOW_DATA_ARRAY)[28].vote_average).toBe(7.669);
+    });
+    it('properly sorts shows by average rating descending', () => {
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[0].vote_average).toBe(10);
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[1].vote_average).toBe(8.4);
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[2].vote_average).toBe(7.669);
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[30].vote_average).toBe(0);
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[29].vote_average).toBe(0);
+        expect(sortShowsByAvgRatingDesc(SHOW_DATA_ARRAY)[28].vote_average).toBe(0);
     });
 });
--- src/helpers/showSortUtils.ts
+++ src/helpers/showSortUtils.ts
@@ -2,7 +2,6 @@ import { ShowData } from '../types';
 
 /**
  * TODO:
- * - Average Rating [H, L]
  * - Release Date [H, L]
  * - Genre [A-Z]
  */
@@ -29,4 +28,31 @@ const sortShowsByRatingCountDesc = (showData: ShowData[]): ShowData[] => {
     return showData;
 };
 
-export { sortShowsByRatingCountAsc, sortShowsByRatingCountDesc };
+/**
+ * Sort an array of shows based on the average rating.
+ * The lowest average will be first, the highest will be last
+ * @param showData | array of shows to be sorted
+ * @returns | array of sorted shows
+ */
+const sortShowsByAvgRatingAsc = (showData: ShowData[]): ShowData[] => {
+    showData.sort((a, b) => (a.vote_average || 0) - (b.vote_average || 0));
+    return showData;
+};
+
+/**
+ * Sort an array of shows based on the average rating.
+ * The highest average will be first, the lowest will be last
+ * @param showData | array of shows to be sorted
+ * @returns | array of sorted shows
+ */
+const sortShowsByAvgRatingDesc = (showData: ShowData[]): ShowData[] => {
+    showData.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
+    return showData;
+};
+
+export {
+    sortShowsByRatingCountAsc,
+    sortShowsByRatingCountDesc,
+    sortShowsByAvgRatingAsc,
+    sortShowsByAvgRatingDesc,
+};
