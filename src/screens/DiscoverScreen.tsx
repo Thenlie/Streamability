b039342a1946ec438504bc1ee90b379051474c48
maxh1231 <maxhumpherys@gmail.com>
2023-10-11 10:53:02 -0600

    state setup, filter import and implementation

--- src/screens/DiscoverScreen.tsx
+++ src/screens/DiscoverScreen.tsx
@@ -1,30 +1,54 @@
-import { ShowCard } from '../components';
+import { useState, useEffect } from 'react';
 import { useProfileContext, useTrendingShows } from '../hooks';
+import { Typography as Typ } from '@mui/material';
+import { ShowCarousel } from '../components';
+import { ShowData } from '../types/tmdb';
+import {
+    filterShowsByAvgRatingAbove,
+    filterShowsByGenre,
+} from '../helpers';
 /**
  * Requests trending movies, passing data to ShowCard components.
  * @returns {JSX.Element}
  */
 export default function DiscoverScreen(): JSX.Element {
     const { profile, setProfile } = useProfileContext();
+    // const [newlyAdded, setNewlyAdded] = useState<ShowData[] | null>(null);
+
+    // Shows
     const { trendingShows, loading } = useTrendingShows('alpha');
+    const [highestRated, setHighestRated] = useState<ShowData[] | null>(null);
+    const [comedy, setComedy] = useState<ShowData[] | null>(null);
+
+    // Movies
+
+    // TV
+
+    useEffect(() => {
+        if (trendingShows) {
+            const avgRatingAbove: ShowData[] = filterShowsByAvgRatingAbove(trendingShows, 4);
+            setHighestRated(avgRatingAbove);
+            // const newlyAdded: showData[] = filterShowsByReleaseAfter()
+            const showsByComedy: ShowData[] = filterShowsByGenre(trendingShows, 35);
+            setComedy(showsByComedy);
+        }
+    }, []);
+
+    console.log(trendingShows);
 
     // TODO: #194 Make skeleton loading screen
     if (loading) return <p>Loading...</p>;
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-6'>
-            {trendingShows?.map(
-                (item, i) =>
-                    item && (
-                        <ShowCard
-                            key={i}
-                            details={item}
-                            showType={item.media_type}
-                            profile={profile}
-                            setProfile={setProfile}
-                        />
-                    )
-            )}
+        <div className=''>
+            <div className='w-full flex flex-col justify-center items-center'>
+                <Typ>Discover Trending</Typ>
+                <ShowCarousel data={trendingShows}></ShowCarousel>
+            </div>
+
+            <div>
+                <Typ>Highest Rated</Typ>
+            </div>
         </div>
     );
 }
