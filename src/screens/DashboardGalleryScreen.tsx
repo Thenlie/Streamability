0fd0948ebb1bb91db4ef63d4266e20ba1d4a6ff1
Leithen Crider <lcrider@cvent.com>
2023-10-14 12:32:31 -0600

    #574 create basic gallery screen

--- src/screens/DashboardGalleryScreen.tsx
+++ src/screens/DashboardGalleryScreen.tsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from 'react-router-dom';
 import { ProfileArrayCols } from '../types';
+import { useGetProfileArray, useProfileActions, useProfileContext } from '../hooks';
+import { ShowPoster } from '../components';
+import { ShowDetailsLoader } from './loaders';
+import { Typography as Typ } from '@mui/material';
 
 export async function loader({ request }: { request: Request }): Promise<string> {
     // get the end of the path from the URL
@@ -12,9 +16,37 @@ export async function loader({ request }: { request: Request }): Promise<string>
 }
 
 const DashboardGalleryScreen: React.FC = () => {
-    const path: string = useLoaderData() as ProfileArrayCols;
+    const { profile, setProfile } = useProfileContext();
+    const profileActions = useProfileActions(profile, setProfile);
+    const path: ProfileArrayCols = useLoaderData() as ProfileArrayCols;
+    const data = useGetProfileArray(path);
 
-    return <p>{path}</p>;
+    // TODO: Create new loader
+    if (!profile) return <ShowDetailsLoader />;
+
+    return (
+        <div className='m-4'>
+            <Typ variant='h5'>
+                {profile.username}&apos;s {path}{' '}
+            </Typ>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-6'>
+                {data?.map(
+                    (item, i) =>
+                        item && (
+                            <ShowPoster
+                                key={i}
+                                details={item}
+                                profile={profile}
+                                profileActions={profileActions}
+                                showQueueButton={path === 'queue'}
+                                showFavoritesButton={path === 'favorites'}
+                                showWatchedButton={path === 'watched'}
+                            />
+                        )
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default DashboardGalleryScreen;
