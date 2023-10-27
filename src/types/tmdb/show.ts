9d66eb037f99d33215d618b2bf363df2bf658127
maxh1231 <maxhumpherys@gmail.com>
2023-10-26 20:34:21 -0600

    Implement num seasons for tv shows. Modified interfaces. #Closes #653

--- src/types/tmdb/show.ts
+++ src/types/tmdb/show.ts
@@ -111,6 +111,18 @@ export interface ShowData {
     }[];
     media_type: 'movie' | 'tv' | 'person';
     genre_ids?: number[];
+    seasons?: [
+        {
+            air_date: string;
+            episode_count: number;
+            id: number;
+            name: string;
+            overview: string;
+            poster_path: string;
+            season_number: number;
+            vote_average: number;
+        },
+    ];
 }
 
 /**
