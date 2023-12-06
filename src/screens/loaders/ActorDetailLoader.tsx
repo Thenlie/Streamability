1959c5cb14f66d6cba8e724a24d28595a617cd37
Leithen Crider <lcrider@cvent.com>
2023-12-05 20:34:14 -0700

    #758 improve actor detail screen loader

--- src/screens/loaders/ActorDetailLoader.tsx
+++ src/screens/loaders/ActorDetailLoader.tsx
@@ -1,32 +1,31 @@
 import Skeleton from 'react-loading-skeleton';
-import { ProvidersLoader, ShowCarouselLoader } from '../../components';
+import { ShowCarouselLoader } from '../../components';
 import React from 'react';
 
 const ActorDetailLoader: React.FC = (): JSX.Element => {
     return (
         <>
-            <section className='m-6 flex flex-col md:flex-row'>
-                <div className='rounded-md m-auto'>
-                    <Skeleton width={350} height={550} />
+            <section className='m-6 flex flex-col lg:flex-row'>
+                <div className='rounded-md text-left lg:mx-auto lg:mt-3'>
+                    <Skeleton
+                        width={window.innerWidth > 1350 ? 375 : 250}
+                        height={window.innerWidth > 1350 ? 525 : 375}
+                    />
                 </div>
-                <div className='m-3 max-w-xl text-left'>
+                <div className='mt-3 lg:ml-3 max-w-xl text-left'>
                     <div className='pb-1'>
-                        <Skeleton width={300} height={70} />
+                        <Skeleton width={300} height={50} />
                     </div>
                     <Skeleton width={180} height={20} />
                     <Skeleton width={80} height={20} />
-                    <Skeleton width={120} height={20} />
-                    <div className='py-2'>
-                        <Skeleton width={180} height={20} />
-                        <Skeleton width={180} height={20} />
-                    </div>
                     <div className='py-3'>
-                        <Skeleton width={500} height={200} />
+                        <Skeleton width={500} height={335} />
                     </div>
-                    <ProvidersLoader count={4} />
                 </div>
             </section>
-            <ShowCarouselLoader />
+            <div className='m-6'>
+                <ShowCarouselLoader />
+            </div>
         </>
     );
 };
