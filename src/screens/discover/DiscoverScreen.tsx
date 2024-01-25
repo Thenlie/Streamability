0500ebb43eba6a907ccfdedebfc400742c0b30ed
Leithen Crider <lcrider@cvent.com>
2024-01-24 23:44:02 -0700

    moved discover request to new file

--- src/screens/discover/DiscoverScreen.tsx
+++ src/screens/discover/DiscoverScreen.tsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useTrendingShows } from '../../hooks';
 import { ShowCarousel, Banner, OfflineSnackbar } from '../../components';
-import { ShowData, DiscoverMovie, DiscoverTv } from '../../types/tmdb';
-import { getDiscoverMovies, getDiscoverTv } from '../../helpers';
+import { ShowData } from '../../types/tmdb';
 import { useNavigate } from 'react-router-dom';
+import {
+    actionAdventureHandler,
+    comedyHandler,
+    highRatedHandler,
+    horrorHandler,
+    huluHandler,
+    netflixHandler,
+    newlyAddedHandler,
+    primeHandler,
+} from './discoverRequests';
+
 /**
  * Requests a variety of filtered shows, rendering carousels
- * @returns {JSX.Element}
  */
 const DiscoverScreen: React.FC = () => {
     const { trendingShows, loading } = useTrendingShows('alpha');
@@ -29,131 +38,18 @@ const DiscoverScreen: React.FC = () => {
     const [popularPrimeLoading, setPopularPrimeLoading] = useState<boolean>(true);
     const [popularHuluLoading, setPopularHuluLoading] = useState<boolean>(true);
 
-    const discoverHandler = async (
-        movieParams: DiscoverMovie,
-        tvParams: DiscoverTv,
-        setState: React.Dispatch<React.SetStateAction<ShowData[] | null>>,
-        setLoading: React.Dispatch<React.SetStateAction<boolean>>
-    ) => {
-        const movies: ShowData[] | null = await getDiscoverMovies(movieParams);
-        const tv: ShowData[] | null = await getDiscoverTv(tvParams);
-        const shows: ShowData[] | null = [];
-
-        if (movies && tv) shows.push(...movies, ...tv);
-        setState(shows);
-        setLoading(false);
-    };
-
     useEffect(() => {
-        const highRatedHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                pages: 1,
-                sort_by: 'popularity.desc',
-                vote_average_gte: 8.0,
-                vote_count_gte: 2000,
-            };
-            discoverHandler(params, params, setHighestRated, setHighestRatedLoading);
-        };
-
-        const newlyAddedHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                pages: 1,
-                sort_by: 'popularity.desc',
-                vote_count_gte: 2000,
-                // TODO: #613 Dynamic date range
-                release_date_gte: '2023-01-01',
-            };
-            discoverHandler(params, params, setNewlyAdded, setNewlyAddedLoading);
-        };
-
-        const actionAdventureHandler = async () => {
-            const movieParams: DiscoverMovie = {
-                include_adult: false,
-                include_video: false,
-                pages: 1,
-                with_genres: '28,12',
-                sort_by: 'popularity.desc',
-                vote_average_gte: 5.0,
-                vote_count_gte: 2000,
-            };
-            const tvParams: DiscoverTv = {
-                include_adult: false,
-                pages: 1,
-                with_genres: '10759',
-                sort_by: 'popularity.desc',
-                vote_average_gte: 5.0,
-                vote_count_gte: 2500,
-            };
-            discoverHandler(movieParams, tvParams, setActionAdventure, setActionAdventureLoading);
-        };
-
-        const comedyHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                include_adult: false,
-                include_video: false,
-                pages: 1,
-                with_genres: '35',
-                sort_by: 'popularity.desc',
-                vote_average_gte: 5.0,
-                vote_count_gte: 2500,
-            };
-            discoverHandler(params, params, setComedy, setComedyLoading);
-        };
-
-        const horrorHandler = async () => {
-            // Only Movie
-            const movieParams: DiscoverMovie = {
-                pages: 1,
-                with_genres: '27',
-                sort_by: 'popularity.desc',
-                vote_average_gte: 5.0,
-                vote_count_gte: 2500,
-            };
-            const HorrorMovies = await getDiscoverMovies(movieParams);
-            setHorror(HorrorMovies);
-            setHorrorLoading(false);
-        };
-
-        const netflixHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                pages: 1,
-                vote_average_gte: 7.5,
-                vote_count_gte: 2500,
-                watch_region: 'US',
-                with_watch_providers: '8',
-            };
-            discoverHandler(params, params, setPopularNetflix, setPopularNetflixLoading);
-        };
-
-        const primeHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                pages: 1,
-                vote_average_gte: 7.5,
-                vote_count_gte: 2500,
-                watch_region: 'US',
-                with_watch_providers: '9',
-            };
-            discoverHandler(params, params, setPopularPrime, setPopularPrimeLoading);
-        };
-
-        const huluHandler = async () => {
-            const params: DiscoverMovie | DiscoverTv = {
-                pages: 1,
-                vote_average_gte: 7.5,
-                vote_count_gte: 2500,
-                watch_region: 'US',
-                with_watch_providers: '15',
-            };
-            discoverHandler(params, params, setPopularHulu, setPopularHuluLoading);
-        };
-        highRatedHandler();
-        newlyAddedHandler();
-        actionAdventureHandler();
-        comedyHandler();
-        horrorHandler();
-        netflixHandler();
-        primeHandler();
-        huluHandler();
+        highRatedHandler({ setState: setHighestRated, setLoading: setHighestRatedLoading });
+        newlyAddedHandler({ setState: setNewlyAdded, setLoading: setNewlyAddedLoading });
+        actionAdventureHandler({
+            setState: setActionAdventure,
+            setLoading: setActionAdventureLoading,
+        });
+        comedyHandler({ setState: setComedy, setLoading: setComedyLoading });
+        horrorHandler({ setState: setHorror, setLoading: setHorrorLoading });
+        netflixHandler({ setState: setPopularNetflix, setLoading: setPopularNetflixLoading });
+        primeHandler({ setState: setPopularHulu, setLoading: setPopularHuluLoading });
+        huluHandler({ setState: setPopularPrime, setLoading: setPopularPrimeLoading });
     }, []);
 
     const sections = [
