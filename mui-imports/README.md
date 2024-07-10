99796f31c4b93a89688026448779058b8cba8e11
Thenlie <leithen113@gmail.com>
2024-07-09 21:36:19 -0600

    create mui-import codemod

---
+++ mui-imports/README.md
@@ -0,0 +1,3 @@
+# MUI import updater
+
+This is a codemod to update MUI default import to named imports
---
+++ mui-imports/src/index.ts
@@ -0,0 +1,23 @@
+import type { Api } from '@codemod.com/workflow';
+
+export async function workflow({ files }: Api) {
+    await files('**/*.{ts,tsx}')
+        .jsFam()
+        // eslint-disable-next-line
+        .astGrep("import { $$$IMPORTS } from '@mui/icons-material';", async ({ replace, map }) => {
+            const newImports: string[] = [];
+            await map(async ({ getMultipleMatches }) => {
+                // get all imports matched with $$$ selector
+                const imports = getMultipleMatches('IMPORTS');
+                // loop through inputs
+                imports.forEach((node) => {
+                    const text = node.text();
+                    // ignore commas
+                    if (text !== ',') {
+                        newImports.push(`import ${text} from '@mui/icons-material/${text}';`);
+                    }
+                });
+            });
+            await replace(newImports.join('\n'));
+        });
+}
