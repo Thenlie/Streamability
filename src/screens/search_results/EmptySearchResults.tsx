f198d0d25558d2318700c9facab4ee4c5b1a7bca
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2024-10-21 09:03:01 -0400

    chore: rename and move filter options component

--- src/screens/search_results/EmptySearchResults.tsx
+++ src/screens/search_results/EmptySearchResults.tsx
@@ -4,7 +4,7 @@ import { ShowCarousel, Button } from '../../components';
 import { useTrendingShows } from '../../hooks';
 import Home from '@mui/icons-material/Home';
 import { useNavigate } from 'react-router-dom';
-import SearchResultsHeader from './SearchResultsHeader';
+import SearchResultsHeader from '../../components/SortFilterHeader';
 
 interface EmptySearchResultsProps {
     /**
--- src/screens/search_results/index.ts
+++ src/screens/search_results/index.ts
@@ -1,6 +1,6 @@
 import EmptySearchResults from './EmptySearchResults';
 import SearchResultsCards from './SearchResultsCards';
-import SearchResultsHeader from './SearchResultsHeader';
+import SearchResultsHeader from '../../components/SortFilterHeader';
 import SearchResultsScreen from './SearchResultsScreen';
 
 export { EmptySearchResults, SearchResultsCards, SearchResultsHeader, SearchResultsScreen };
