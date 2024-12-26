e61fe0126aad9ca50c7e7458c338242287d94124
Richie911 <oohrichmond.k@gmail.com>
2024-12-26 17:31:28 +0000

    feat: restructure crew section rendering in ShowDetailsScreen

--- src/screens/ShowDetailsScreen.tsx
+++ src/screens/ShowDetailsScreen.tsx
@@ -202,24 +202,24 @@ const ShowDetailsScreen: React.FC = () => {
                 </div>
             </section>
             {details.credits && details.credits.cast.length > 0 && (
-                <>
-                    <section className='w-full px-6 md:px-12'>
-                        <Typ variant='h5'>Cast</Typ>
-                        <div className='flex flex-nowrap overflow-x-auto w-full'>
-                            {details.credits.cast.map((actor, i) => {
-                                return <ActorCard key={i} details={actor} />;
-                            })}
-                        </div>
-                    </section>
-                    <section className='w-full px-6 md:px-12 mt-3'>
-                        <Typ variant='h5'>Crew</Typ>
-                        <div className='flex flex-nowrap overflow-x-auto w-full'>
-                            {details.credits.crew.map((actor, i) => {
-                                return <ActorCard key={i} details={actor} />;
-                            })}
-                        </div>
-                    </section>
-                </>
+                <section className='w-full px-6 md:px-12'>
+                    <Typ variant='h5'>Cast</Typ>
+                    <div className='flex flex-nowrap overflow-x-auto w-full'>
+                        {details.credits.cast.map((actor, i) => {
+                            return <ActorCard key={i} details={actor} />;
+                        })}
+                    </div>
+                </section>
+            )}
+            {details.credits && details.credits.crew.length > 0 && (
+                <section className='w-full px-6 md:px-12 mt-3'>
+                    <Typ variant='h5'>Crew</Typ>
+                    <div className='flex flex-nowrap overflow-x-auto w-full'>
+                        {details.credits.crew.map((actor, i) => {
+                            return <ActorCard key={i} details={actor} />;
+                        })}
+                    </div>
+                </section>
             )}
             <section className='py-8'>
                 <ShowCarousel
