787eff2c24f60368a2c7ccb5c61baaeeae7b8505
MaxHumpherys <maxhumpherys@gmail.com>
2025-01-22 22:00:59 -0700

    SeasonCard loaders and stories

---
+++ src/components/loaders/SeasonCardLoader.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+interface SeasonCardLoaderProps {
+    /**
+     * Number of skeleton loaders to display
+     */
+    count: number;
+}
+
+/**
+ * A skeleton loader of the season card component. To be rendered while
+ * main component is loading.
+ * @param count | number of card placeholders to be rendered
+ * @returns {JSX.Element}
+ */
+const SeasonCardLoader: React.FC<SeasonCardLoaderProps> = ({ count }): JSX.Element => {
+    return (
+        <>
+            {[...Array(count)].map((x, i) => (
+                <div key={i} className='flex flex-row'>
+                    <Skeleton width={180} height={270} />
+                    <div className='p-4 flex flex-col'>
+                        <Skeleton className='my-2' width={150} count={1} />
+                        <Skeleton className='my-2' width={275} count={1} />
+                        <Skeleton className='my-2' width={400} height={150} count={1} />
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default SeasonCardLoader;
