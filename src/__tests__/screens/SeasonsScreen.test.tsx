e7ce650f0dd4e76a0b8d1a1cddc5659dc959c78f
MaxHumpherys <maxhumpherys@gmail.com>
2025-02-17 00:01:31 -0700

    validation function to remove unwanted seasons, render SeasonCard on ShowDetails, implement SeasonScreen accessing datavia router instead of API if possible, misc style changes and code cleanup

--- src/__tests__/screens/SeasonsScreen.test.tsx
+++ src/__tests__/screens/SeasonsScreen.test.tsx
@@ -1,20 +1,11 @@
 import '@testing-library/jest-dom';
 import { describe, it, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { Router, RouterProvider, createMemoryRouter } from 'react-router';
+import { RouterProvider, createMemoryRouter } from 'react-router';
 import { routes } from '../../routes';
-import {
-    getMovieDetails,
-    getMovieRecommendations,
-    getReleaseDate,
-    getRuntime,
-    getTvDetails,
-    getTvRecommendations,
-} from '../../helpers';
+import { getTvDetails } from '../../helpers';
 import { act } from 'react';
-import { MOVIE_DETAIL, TRENDING_DATA, TV_DETAIL } from '../constants';
-import { ShowData } from '../../types';
-import { createMemoryHistory } from 'history';
+import { TV_DETAIL } from '../constants';
 
 const TMDB_BASE_PATH = 'https://image.tmdb.org/t/p/w500';
 
@@ -23,7 +14,7 @@ vi.mock('../../helpers', async () => {
 
     return {
         ...(actual as object),
-        getTvDetails: vi.fn()
+        getTvDetails: vi.fn(),
     };
 });
 
@@ -35,35 +26,35 @@ describe('Seasons Screen', () => {
     it('properly renders applicable TV show data', async () => {
         vi.mocked(getTvDetails).mockResolvedValue(TV_DETAIL);
 
-        render(
-            <RouterProvider router={tvRouter} />
-        );
+        render(<RouterProvider router={tvRouter} />);
 
-        await screen.findByTestId('seasons-screen')
+        await screen.findByTestId('seasons-screen');
         expect(screen.getByText(TV_DETAIL.title)).toBeInTheDocument();
-        expect(screen.getByText("(" + TV_DETAIL.release_date?.slice(0, 4) + ")"))
-        expect(screen.getByAltText(`${TV_DETAIL.title} poster`)).toHaveAttribute('src', TMDB_BASE_PATH + TV_DETAIL.poster_path);
-    })
+        expect(screen.getByText('(' + TV_DETAIL.release_date?.slice(0, 4) + ')'));
+        expect(screen.getByAltText(`${TV_DETAIL.title} poster`)).toHaveAttribute(
+            'src',
+            TMDB_BASE_PATH + TV_DETAIL.poster_path
+        );
+    });
     it('properly renders placeholder image if no poster path is provided', async () => {
         vi.mocked(getTvDetails).mockResolvedValue({ ...TV_DETAIL, poster_path: null });
-        render(
-            <RouterProvider router={tvRouter} />
-        )
+        render(<RouterProvider router={tvRouter} />);
 
-        await screen.findByTestId('seasons-screen')
-        expect(screen.getByAltText(`${TV_DETAIL.title} poster`)).toHaveAttribute('src', '/poster-placeholder.jpeg')
-    })
-    it('properly provides link that navigates back to TV details', async () => {
+        await screen.findByTestId('seasons-screen');
+        expect(screen.getByAltText(`${TV_DETAIL.title} poster`)).toHaveAttribute(
+            'src',
+            '/poster-placeholder.jpeg'
+        );
+    });
+    it('page header properly provides link that navigates back to TV details', async () => {
         vi.mocked(getTvDetails).mockResolvedValue(TV_DETAIL);
 
-        render(
-            <RouterProvider router={tvRouter} />
-        );
+        render(<RouterProvider router={tvRouter} />);
 
-        await screen.findByTestId('seasons-screen')
+        await screen.findByTestId('seasons-screen');
         await act(async () => {
             fireEvent.click(await screen.findByRole('link', { name: 'Back' }));
-        })
-        expect(tvRouter.state.location.pathname).toBe(`/details/tv/${TV_DETAIL.id}`)
-    })
-})
+        });
+        expect(tvRouter.state.location.pathname).toBe(`/details/tv/${TV_DETAIL.id}`);
+    });
+});
